afd8f53432e7b061b9feaa91eb1e242b10e2e95e
Ryan Johnston <ryanj@ryanj.org>
2019-12-20 10:49:59 -0700

    Update with better aligned variable name

--- Model/ReachConfigProvider.php
+++ Model/ReachConfigProvider.php
@@ -62,7 +62,7 @@ class ReachConfigProvider implements ConfigProviderInterface
         $config['reach']['dhl_quote']=$this->getDhlQuote();
         $config['reach']['fingerprint_url']=$this->getFingerPrintUrl();
         $config['reach']['dhl_label']=$this->reachHelper->getDhlDutyLabel();
-        $config['reach']['api_url']=$this->reachHelper->getStashApiUrl();
+        $config['reach']['stash_url']=$this->reachHelper->getStashApiUrl();
         return $config;
     }
 
