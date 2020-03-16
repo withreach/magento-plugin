2cd3c3ed80ed1342fd4446d9bb06386d5b7b4138
suraiyavic <suraiya@withreach.com>
2020-03-16 08:48:40 -0700

    Cleanup as per Code Review Feedback

--- Model/Currency.php
+++ Model/Currency.php
@@ -219,7 +219,6 @@ class Currency extends \Magento\Framework\Model\AbstractModel
      */
     protected function checkLocalIP($ip)
     {
-        //Ryan's code from MAG-75 (to be able to test payment code properly)
         if (!filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE))
         {
             // is a local ip address
