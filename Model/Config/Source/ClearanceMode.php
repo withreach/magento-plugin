918a35ff085f1d87922d27323db16299b09c7ccf
Suraiya Khan <suraiya@withreach.com>
2020-03-19 17:03:01 -0700

    [MAG-112] Fix for Unspecified Option in the Clearance Mode Dropdown

--- Model/Config/Source/ClearanceMode.php
+++ Model/Config/Source/ClearanceMode.php
@@ -12,10 +12,18 @@ class ClearanceMode implements \Magento\Framework\Option\ArrayInterface
      */
     public function toOptionArray()
     {
+        /*We are setting value to 'N/A' instead of '' or ' '
+        that is because by the builtin method that saves data in the config table ; the '' is getting saved as null in
+        the database. Now on retrieval that null causes problem. This is because if a config is null at a more specific
+        scope like store then the logic looks for a value in the parent scope instead of stopping at that more specific
+        scope/level.
+        There are several ways to solve this. The shortest route that seemed to worked both in the database, dynamic
+        scope control logic and integrating with DHL is this 'N/A' value.
+        */
         return [
             ['value' => 'COURIER', 'label' => __('Courier')],
             ['value' => 'POST', 'label' => __('Post')],
-            ['value' => '', 'label' => __('No Selection')]
+            ['value' => 'N/A', 'label' => __('No Selection')]
         ];
     }
 }
