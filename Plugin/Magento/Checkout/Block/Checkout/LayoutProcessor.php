7773de5ad3c6d1d0c4894180cf3b1d2f9e007757
Suraiya Khan <suraiya@withreach.com>
2020-04-27 10:54:42 -0700

    [MAG-86] Added Helpful Hints
    Copied a helpful hint/link from the JIRA ticket MAG-86 and pasted in the source
     code so that developers would know why the work was done in that particular way.

--- Plugin/Magento/Checkout/Block/Checkout/LayoutProcessor.php
+++ Plugin/Magento/Checkout/Block/Checkout/LayoutProcessor.php
@@ -9,6 +9,7 @@ class LayoutProcessor
     /**
      * Process js Layout of block.
      * UI item is getting removed
+     * https://devdocs.magento.com/guides/v2.3/howdoi/checkout/checkout_customize.html#remove
      * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
      * @param \Closure $proceed
      * @param array $jsLayout
